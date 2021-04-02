164b716ac1c8b56623a21734d02d9cb83eb76929
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-04-02 20:47:16 +0200

    progress bar animation, inputs values

--- script/nextForm.js
+++ script/nextForm.js
@@ -3,6 +3,8 @@ const progressCover = document.querySelector(
   ".progress-container__line--cover"
 );
 const progressDot2 = document.querySelector(".progress-container__dots--2");
+const progressIcon2 = document.querySelector(".progress-container__icons--2")
+  .parentElement;
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -10,6 +12,14 @@ submitBtn.addEventListener("click", (e) => {
   progressCover.style.width = "45%";
   progressCover.addEventListener("transitionend", () => {
     progressDot2.style = "background-color: #0581aa";
-    // console.log(progressDot2);
+    iconColor = progressIcon2.childNodes[0].childNodes[0];
+    iconColor.setAttribute("fill", "#ffffff");
   });
+  //FORM 1 VALUES
+  const formInputs = document.querySelectorAll(".form__input");
+  const formValues = [];
+  formInputs.forEach((input) => {
+    formValues.push(input.value);
+  });
+  console.log(formValues);
 });
