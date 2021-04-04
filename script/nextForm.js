cc0ce48220a7aab2599521cb267cacf0339993e3
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-04-04 23:20:13 +0200

    progress bar form switching

--- script/nextForm.js
+++ script/nextForm.js
@@ -1,14 +1,19 @@
-const submitBtn = document.querySelector(".form__input--button");
+const submitBtn1 = document.querySelector(".form__input--button");
 const progressCover = document.querySelector(
   ".progress-container__line--cover"
 );
 const progressDot2 = document.querySelector(".progress-container__dots--2");
 const progressIcon2 = document.querySelector(".progress-container__icons--2")
   .parentElement;
+const progressDot3 = document.querySelector(".progress-container__dots--3");
+const progressIcon3 = document.querySelector(".progress-container__icons--3")
+  .parentElement;
 const form1 = document.querySelector(".form");
 const form2 = document.querySelector(".form2");
+const form3 = document.querySelector(".form3");
+const progressDots = document.querySelectorAll(".progress-container__dots");
 
-submitBtn.addEventListener("click", (e) => {
+submitBtn1.addEventListener("click", (e) => {
   e.preventDefault();
   //PROGRESS BAR
   progressCover.style.width = "45%";
@@ -27,3 +32,68 @@ submitBtn.addEventListener("click", (e) => {
   form1.classList.add("form--unactive");
   form2.classList.remove("form2--unactive");
 });
+
+progressDots.forEach((dot) => {
+  dot.addEventListener("click", barChange);
+});
+
+function barChange() {
+  if (this.classList.contains("progress-container__dots--1")) {
+    form1.classList.remove("form--unactive");
+    form2.classList.add("form2--unactive");
+    form3.classList.add("form3--unactive");
+
+    progressCover.style.width = "25%";
+    progressDot2.style = "background-color: #c0bdbd";
+    iconColor2 = progressIcon2.childNodes[0].childNodes[0];
+    iconColor2.setAttribute("fill", "#000000");
+    progressDot3.style = "background-color: #c0bdbd";
+    iconColor3 = progressIcon3.childNodes[0].childNodes[0];
+    iconColor3.setAttribute("fill", "#000000");
+    //BUG FIX
+    progressCover.addEventListener("transitionend", () => {
+      progressDot2.style = "background-color: #c0bdbd";
+      iconColor = progressIcon2.childNodes[0].childNodes[0];
+      iconColor.setAttribute("fill", "#000000");
+      progressDot3.style = "background-color: #c0bdbd";
+      iconColor3 = progressIcon3.childNodes[0].childNodes[0];
+      iconColor3.setAttribute("fill", "#000000");
+    });
+  } else if (this.classList.contains("progress-container__dots--2")) {
+    form1.classList.add("form--unactive");
+    form2.classList.remove("form2--unactive");
+    form3.classList.add("form3--unactive");
+
+    progressCover.style.width = "45%";
+    progressDot3.style = "background-color: #c0bdbd";
+    iconColor3 = progressIcon3.childNodes[0].childNodes[0];
+    iconColor3.setAttribute("fill", "#000000");
+    progressCover.addEventListener("transitionend", () => {
+      progressDot2.style = "background-color: #0581aa";
+      iconColor = progressIcon2.childNodes[0].childNodes[0];
+      iconColor.setAttribute("fill", "#ffffff");
+      //BUG FIX
+      progressDot3.style = "background-color: #c0bdbd";
+      iconColor3 = progressIcon3.childNodes[0].childNodes[0];
+      iconColor3.setAttribute("fill", "#000000");
+    });
+  } else if (this.classList.contains("progress-container__dots--3")) {
+    form1.classList.add("form--unactive");
+    form2.classList.add("form2--unactive");
+    form3.classList.remove("form3--unactive");
+
+    progressCover.style.width = "95%";
+    progressDot2.style = "background-color: #0581aa";
+    iconColor2 = progressIcon2.childNodes[0].childNodes[0];
+    iconColor2.setAttribute("fill", "#ffffff");
+    progressCover.addEventListener("transitionend", () => {
+      progressDot3.style = "background-color: #0581aa";
+      iconColor = progressIcon3.childNodes[0].childNodes[0];
+      iconColor.setAttribute("fill", "#ffffff");
+      //BUG FIX
+      progressDot2.style = "background-color: #0581aa";
+      iconColor = progressIcon2.childNodes[0].childNodes[0];
+      iconColor.setAttribute("fill", "#ffffff");
+    });
+  }
+}
