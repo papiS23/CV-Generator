75570d47b293624b4b8ec12f71cfd0678893343b
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-04-06 17:20:25 +0200

    second form- js

--- script/addElement.js
+++ script/addElement.js
@@ -1,40 +1,59 @@
 const addBtn = document.querySelectorAll(".experience__input--button");
-const containerOne = document.querySelector(".experience__container--one");
+const containerOne = document.querySelectorAll(".experience__container");
 
-addBtn.forEach((thisBtn) =>
-  thisBtn.addEventListener("click", function (event) {
-    event.preventDefault();
+for (let i = 0; i < addBtn.length; i++) {
+  let self = addBtn[i];
 
-    const element = document.createElement("div");
-    element.classList.add("element");
+  self.addEventListener(
+    "click",
+    function (event) {
+      event.preventDefault();
+      createBar(i);
+    },
+    false
+  );
+}
 
-    const text = document.createElement("p");
-    text.classList.add("element__text");
-    const professionValue = document.querySelector("#profession").value;
-    const cityValue = document.querySelector("#city").value;
-    const companyValue = document.querySelector("#company").value;
-    const startValue = document.querySelector("#start").value;
-    const endValue = document.querySelector("#end").value;
-    const selectValue = document.querySelector("#select").value;
+function createBar(i) {
+  const element = document.createElement("div");
+  element.classList.add("element");
 
-    const delBtn = document.createElement("button");
-    delBtn.classList.add("element__button");
-    delBtn.innerText = "X";
+  console.log(i);
+  console.log(containerOne);
 
-    text.innerText = `${professionValue == "" ? "" : professionValue}, ${
-      companyValue == "" ? "" : companyValue
-    } - ${cityValue == "" ? "" : cityValue} ${
-      selectValue == "" ? "" : selectValue
-    } ${startValue == "" ? "" : startValue} ${endValue == "" ? "" : endValue} `;
+  const text = document.createElement("p");
+  text.classList.add("element__text");
+  const professionValue = document.querySelectorAll(
+    ".experience__input--profession"
+  ).value;
+  const cityValue = document.querySelectorAll(".experience__input--city").value;
+  const companyValue = document.querySelectorAll(".experience__input--company")
+    .value;
+  const startValue = document.querySelectorAll(".experience__input--start")
+    .value;
+  const endValue = document.querySelectorAll(".experience__input--end").value;
+  const selectValue = document.querySelectorAll(".experience__input--select")
+    .value;
 
-    console.log(text);
-    element.appendChild(text);
-    element.appendChild(delBtn);
+  const delBtn = document.createElement("button");
+  delBtn.classList.add("element__button");
+  delBtn.innerText = "X";
 
-    delBtn.addEventListener("click", function () {
-      element.remove();
-    });
+  text.innerText = `${professionValue[i] == "" ? "" : professionValue[i]}, ${
+    companyValue[i] == "" ? "" : companyValue[i]
+  } - ${cityValue[i] == "" ? "" : cityValue[i]} ${
+    selectValue[i] == "" ? "" : selectValue[i]
+  } ${startValue[i] == "" ? "" : startValue[i]} ${
+    endValue[i] == "" ? "" : endValue[i]
+  } `;
 
-    containerOne.appendChild(element);
-  })
-);
+  console.log(text);
+  element.appendChild(text);
+  element.appendChild(delBtn);
+
+  delBtn.addEventListener("click", function () {
+    element.remove();
+  });
+
+  containerOne[i].appendChild(element);
+}
