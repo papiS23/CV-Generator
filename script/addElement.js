3dc6a9cdfa7ed7ba8d204367ab9db7694aa69870
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-04-07 10:31:05 +0200

    second form - js done

--- script/addElement.js
+++ script/addElement.js
@@ -1,59 +1,67 @@
-const addBtn = document.querySelectorAll(".experience__input--button");
-const containerOne = document.querySelectorAll(".experience__container");
+const addBtn = document.querySelectorAll(".experience__input--button"); // pobranie przycisków 
+const containerOne = document.querySelectorAll(".experience__container"); // pobranie kontenera
 
-for (let i = 0; i < addBtn.length; i++) {
-  let self = addBtn[i];
+for (let i = 0; i < addBtn.length; i++) { // pętla po ilości przycisków == pętla po sekcjach 
+  let self = addBtn[i]; // indeks danego przycisku == indeks danej sekcji
 
-  self.addEventListener(
-    "click",
-    function (event) {
+  self.addEventListener("click", function (event) { 
       event.preventDefault();
-      createBar(i);
+      createBar(i); // wykonaj funkcję z tym danym indeksem
     },
-    false
-  );
+    false);
 }
 
 function createBar(i) {
+  // stworzenie zmiennych
+  let professionValue;
+  let cityValue;
+  let companyValue;
+  let startValue;
+  let endValue;
+  let selectValue; 
+  // stworzenie tego zielonego paska w którym są wyświetlone informacje 
   const element = document.createElement("div");
-  element.classList.add("element");
-
-  console.log(i);
-  console.log(containerOne);
-
-  const text = document.createElement("p");
+  element.classList.add("element"); 
+  // stworzenie tekstu 
+  const text = document.createElement("p"); 
   text.classList.add("element__text");
-  const professionValue = document.querySelectorAll(
-    ".experience__input--profession"
-  ).value;
-  const cityValue = document.querySelectorAll(".experience__input--city").value;
-  const companyValue = document.querySelectorAll(".experience__input--company")
-    .value;
-  const startValue = document.querySelectorAll(".experience__input--start")
-    .value;
-  const endValue = document.querySelectorAll(".experience__input--end").value;
-  const selectValue = document.querySelectorAll(".experience__input--select")
-    .value;
-
+  // przycisk usuń 
   const delBtn = document.createElement("button");
   delBtn.classList.add("element__button");
   delBtn.innerText = "X";
-
-  text.innerText = `${professionValue[i] == "" ? "" : professionValue[i]}, ${
-    companyValue[i] == "" ? "" : companyValue[i]
-  } - ${cityValue[i] == "" ? "" : cityValue[i]} ${
-    selectValue[i] == "" ? "" : selectValue[i]
-  } ${startValue[i] == "" ? "" : startValue[i]} ${
-    endValue[i] == "" ? "" : endValue[i]
-  } `;
-
-  console.log(text);
+  //od indeksu zależy jakie wartości zostaną pobrane, wcześniej jak wypełniałeś jakiś form i tam nie było jakiegoś inputa to wywalało bład w konsoli, teraz do każdego forma są przypisane dane zmienne i nie ma błedu
+    if(i==0){
+      professionValue = document.querySelector(`#profession${i}`).value;
+      cityValue = document.querySelector(`#city${i}`).value;
+      companyValue = document.querySelector(`#company${i}`).value;
+      startValue = document.querySelector(`#start${i}`).value;
+      endValue = document.querySelector(`#end${i}`).value;
+    }else if(i==1){
+      professionValue = document.querySelector(`#profession${i}`).value;
+      cityValue = document.querySelector(`#city${i}`).value;
+      companyValue = document.querySelector(`#company${i}`).value;
+      startValue = document.querySelector(`#start${i}`).value;
+      endValue = document.querySelector(`#end${i}`).value;
+    }else if(i==2){
+      professionValue = document.querySelector(`#profession${i}`).value;
+      selectValue = document.querySelector(`#select${i}`).value;
+    }else if(i==3){
+      professionValue = document.querySelector(`#profession${i}`).value;
+      selectValue = document.querySelector(`#select${i}`).value;
+    }else if(i==4){
+      professionValue = document.querySelector(`#profession${i}`).value;
+    }else{
+      console.log("Nie działa");
+    }
+  // wypisanie zawartości zmiennych do tekstu 
+  text.innerText = `${professionValue == undefined ? '' : professionValue}, ${companyValue == undefined ? '' : companyValue} - ${cityValue == undefined ? '' : cityValue} ${selectValue == undefined ? '' : selectValue} ${startValue == undefined ? '' : startValue} ${endValue == undefined ? '' : endValue}`;
+  //dodanie text i delBtn do tego zielonego elementu
   element.appendChild(text);
   element.appendChild(delBtn);
-
+  // przyciks do usuwania 
   delBtn.addEventListener("click", function () {
     element.remove();
   });
-
+  //dodawanie tego zielonego paska do danej sekcji 
   containerOne[i].appendChild(element);
 }
